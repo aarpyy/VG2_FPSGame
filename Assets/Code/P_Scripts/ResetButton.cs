c5785fc19ac580332ce2c808c40c4593fdfa628a
PeytonRod <89867794+PeytonRod@users.noreply.github.com>
2024-04-17 16:17:08 -0500

    Small Change

--- Assets/Code/P_Scripts/ResetButton.cs
+++ Assets/Code/P_Scripts/ResetButton.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using JUTPS.JUInputSystem;
+using JUTPS;
 
 public class ResetButton : MonoBehaviour
 {
+    bool touch = false;
+    [Header("Input")]
+    public InputActionReference InteractInput;
+
+    private void Awake()
+    {
+        InteractInput.action.performed += OnInteract;
+    }
     // Start is called before the first frame update
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Keyboard.current.fKey.isPressed)
-            {
-                SceneManager.LoadScene("Practice Range");
-            }
+            touch = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        touch = false;
+    }
+
+    private void OnInteract(InputAction.CallbackContext ctx) {
+
+        if (touch == true) {
+
+            SceneManager.LoadScene("Practice Range");
         }
     }
 
-    //public void onInteract(InputValue value)
-    //{
 
-    //}
 }
